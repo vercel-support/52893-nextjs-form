764278753e64cafc5ae6b7dbdbfc825198a31d70
William Li <email@williamli.dev>
2021-11-08 14:16:58 +0800

    added axios call to api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
+import axios from 'axios'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [results, setResults] = useState('');
+
+  const fireAPI = async () => {
+    console.log('calling API...');
+    const response = await axios.get('https://52893-thepipeline.vercel-support.app/api/mail');
+    console.log({response})
+
+    setResults(`${Date.now()} - ${JSON.stringify(response.data)}\r\n${results}`)
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -16,40 +27,14 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p className={styles.trigger}>
+          <button onClick={fireAPI}>
+            Call API
+          </button>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <textarea className={styles.results} value={results} readOnly/>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
       <footer className={styles.footer}>
